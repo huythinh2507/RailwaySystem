99dfe039b0a9f68a18a71395bdc5b7e41c71633a
TTLforwork <113823576+TTLforwork@users.noreply.github.com>
2025-11-26 17:02:34 +0700

    Refactor Feedback class to use primitive types

--- src/main/java/com/railway/model/Feedback.java
+++ src/main/java/com/railway/model/Feedback.java
@@ -1,106 +1,47 @@
 package com.railway.model;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 public class Feedback {
-    private Integer feedbackId;
+    private int feedbackId;
     private String username;
     private String trainNumber;
-    private Integer rating;
-    private String comments;
-    private LocalDateTime submittedDate;
-    private String status;
+    private int rating;
+    private String comment;
     private String adminResponse;
+    private LocalDateTime dateSubmitted;
 
-    public Feedback() {
-    }
+    public Feedback() {}
 
-    public Feedback(String username, String trainNumber, Integer rating, String comments) {
+    // Constructor for submission
+    public Feedback(String username, String trainNumber, int rating, String comment) {
         this.username = username;
         this.trainNumber = trainNumber;
         this.rating = rating;
-        this.comments = comments;
-        this.submittedDate = LocalDateTime.now();
-        this.status = "PENDING";
+        this.comment = comment;
     }
 
-    public Integer getFeedbackId() {
-        return feedbackId;
-    }
+    // --- Getters and Setters ---
+    public int getFeedbackId() { return feedbackId; }
+    public void setFeedbackId(int feedbackId) { this.feedbackId = feedbackId; }
 
-    public void setFeedbackId(Integer feedbackId) {
-        this.feedbackId = feedbackId;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getUsername() {
-        return username;
-    }
+    public String getTrainNumber() { return trainNumber; }
+    public void setTrainNumber(String trainNumber) { this.trainNumber = trainNumber; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public int getRating() { return rating; }
+    public void setRating(int rating) { this.rating = rating; }
 
-    public String getTrainNumber() {
-        return trainNumber;
-    }
+    public String getComment() { return comment; }
+    public void setComment(String comment) { this.comment = comment; }
 
-    public void setTrainNumber(String trainNumber) {
-        this.trainNumber = trainNumber;
-    }
+    public String getAdminResponse() { return adminResponse; }
+    public void setAdminResponse(String adminResponse) { this.adminResponse = adminResponse; }
 
-    public Integer getRating() {
-        return rating;
-    }
-
-    public void setRating(Integer rating) {
-        this.rating = rating;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    public LocalDateTime getSubmittedDate() {
-        return submittedDate;
-    }
-
-    public void setSubmittedDate(LocalDateTime submittedDate) {
-        this.submittedDate = submittedDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getAdminResponse() {
-        return adminResponse;
-    }
-
-    public void setAdminResponse(String adminResponse) {
-        this.adminResponse = adminResponse;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Feedback feedback = (Feedback) o;
-        return Objects.equals(feedbackId, feedback.feedbackId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(feedbackId);
-    }
+    public LocalDateTime getDateSubmitted() { return dateSubmitted; }
+    public void setDateSubmitted(LocalDateTime dateSubmitted) { this.dateSubmitted = dateSubmitted; }
 
     @Override
     public String toString() {
@@ -109,8 +50,8 @@ public class Feedback {
                 ", username='" + username + '\'' +
                 ", trainNumber='" + trainNumber + '\'' +
                 ", rating=" + rating +
-                ", comments='" + comments + '\'' +
-                ", status='" + status + '\'' +
+                ", comment='" + comment + '\'' +
+                ", adminResponse='" + adminResponse + '\'' +
                 '}';
     }
 }
